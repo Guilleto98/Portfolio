1f1a0791d0bc8297cdd6ab17f6e04b6d99966ae2
Guillermo Torres <98137107+Guilleto98@users.noreply.github.com>
2023-05-09 12:43:21 -0300

    Agregue un aboutme2

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Nav from "./components/Nav"
 import Main from "./components/Main"
 import ShowProjects from "./layouts/ShowProjects"
 import Aboutme from "./layouts/Aboutme"
+import Aboutme2 from "./layouts/Aboutme2"
 
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       <Main/>
       <ShowProjects/>
       <Aboutme/>
+      <Aboutme2/>
     </>
   )
 }
--- src/layouts/Aboutme.jsx
+++ src/layouts/Aboutme.jsx
@@ -41,47 +41,6 @@ const Aboutme = () => {
           </div>
         </div>
       </div>
-
-      <div className="flex flex-col md:flex-row items-center justify-center h-screen w-full">
-        <div className="w-full h-full md:w-1/2 bg-gray-700 flex flex-col justify-center p-10">
-          <div className="div-p p-5 rounded-lg md:p-5">
-            <p className="p-Aboutme font-serif leading-relaxed text-3xl text-center md:text-left px-20 py-20">
-              Not only am I passionat e about technology, but I also enjoy
-              living amazing experiences. High-adrenaline sports are my passion.
-              I practice kitesurfing whenever conditions allow, and it's one of
-              the greatest passions I have. I strive to strike a balance between
-              spending all day programming and engaging in extreme sports.
-            </p>
-          </div>
-        </div>
-        <div className="w-full md:w-1/2 h-1/2 md:h-full bg">
-          <img
-            src={me2}
-            alt=""
-            className="w-full h-full object-cover md:h-full"
-          />
-        </div>
-      </div>
-
-      {/* <div className="flex flex-wrap items-center justify-center h-screen w-full">
-        <div className="w-full h-full  md:w-1/2 bg-gray-700 flex items-center justify-center">
-          <div className="div-p m-10 p-5 rounded-lg">
-            <p className="p-Aboutme font-serif leading-relaxed text-3xl text-center md:text-left px-20 py-20">
-              Not only am I passionat e about technology, but I also enjoy living
-              amazing experiences. High-adrenaline sports are my passion. I
-              practice kitesurfing whenever conditions allow, and it's one of
-              the greatest passions I have. I strive to strike a balance between
-              spending all day programming and engaging in extreme sports.
-            </p>
-          </div>
-        </div>
-        <div className="w-full md:w-1/2 h-screen bg-cover bg-center">
-          <img src={me2} alt="" className="w-full h-full object-cover" />
-        </div>
-      </div> 
-      
-      
-      */}
     </>
   );
 };
